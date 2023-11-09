7b9eb8b0068432fa7f34e22c243a5ca650223f75
Maciej Skoczen <maciej.skoczen@avasecutiry.com>
2023-11-09 09:16:44 +0100

    Add lsb algorithms

---
+++ project/algorithms/basic_lsb.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+def coords_from_ctr(ctr, h, w, d):
+    return ctr%8, ctr//8%d, ctr//(8*d)%w, ctr//(8*d*w)%h
+
+def lsb_basic_hide(im1, im2, color_proportion=None):
+    h1, w1, d1 = im1.shape
+    h2, w2, d2 = im2.shape
+    
+    if color_proportion is None:
+        color_proportion = [8*h2//h1]*3
+        
+    im3 = im1.copy()
+    im2_ctr = 0
+    
+    for x1 in range(h1):
+        for y1 in range(w1):
+            for z1 in range(d1):
+                b = im3[x1, y1, z1]
+                p = color_proportion[z1]
+                b = ((b>>p)<<p) # Wipe p LSBits from b
+                for j in range(p - 1, -1, -1):
+                    b2, z2, y2, x2 = coords_from_ctr(im2_ctr, h2, w2, d2)
+                    b = b | (((im2[x2, y2, z2]>>(7 - b2))&1)<<j) # Take b2-th MSB of im2 pixel and insert it at j-th LSB of b
+                    im2_ctr += 1
+                    if im2_ctr == h2*w2*d2*8:
+                        im3[x1, y1, z1] = b
+                        return im3
+                                    
+                im3[x1, y1, z1] = b
+    return im3
+
+def lsb_basic_reveal(im, hidden_shape, color_proportion=None):
+    h1, w1, d1 = im.shape
+    h2, w2, d2 = hidden_shape
+    
+    if color_proportion is None:
+        color_proportion = [8*h2//h1]*3
+        
+    res = np.zeros(hidden_shape, dtype=int)
+    res_b = 0
+    im2_ctr = 0
+    
+    for x1 in range(h1):
+        for y1 in range(w1):
+            for z1 in range(d1):
+                b = im[x1, y1, z1]
+                p = color_proportion[z1]
+                for j in range(p - 1, -1, -1):
+                    b2, z2, y2, x2 = coords_from_ctr(im2_ctr, h2, w2, d2)
+                    res_b = res_b | (((b>>j)&1) << (7 - b2)) # Take j-th LSB of b and insert it at b2-th MSG of res_b
+                    
+                    im2_ctr += 1
+                    if b2 == 7:
+                        res[x2, y2, z2] = res_b
+                        res_b = 0
+                    if im2_ctr == h2*w2*d2*8:
+                        return res
+                                    
+    return res
---
+++ project/algorithms/variable_rate_lsb.py
@@ -0,0 +1,63 @@
+import numpy as np
+
+def coords_from_ctr(ctr, h, w, d):
+    return ctr%8, ctr//8%d, ctr//(8*d)%w, ctr//(8*d*w)%h
+
+def lsb_vr_hide(im1, im2, alpha=9, max_p=4):
+    h1, w1, d1 = im1.shape
+    h2, w2, d2 = im2.shape
+    
+    im3 = im1.copy()
+    
+    im2_ctr = 0
+    
+    for row in range(2, h1 - 1):
+        for col in range(2 + row%2, w1 - 1, 2):
+            for d in range(d1):
+                x = im1[row - 1, col, d] ^ im1[row + 1, col, d] ^ im1[row, col - 1, d] ^ im1[row, col + 1, d]
+                p = 1
+                if x > alpha:
+                    p = min(max_p, int(np.ceil(x/2)))
+                
+                b = im3[row, col, d]
+                b = ((b>>p)<<p) # Wipe p LSBits from b
+                for j in range(p - 1, -1, -1):
+                    b2, z2, y2, x2 = coords_from_ctr(im2_ctr, h2, w2, d2)
+                    b = b | (((im2[x2, y2, z2]>>(7 - b2))&1)<<j) # Take b2-th MSB of im2 pixel and insert it at j-th LSB of b
+                    im2_ctr += 1
+                    if im2_ctr == h2*w2*d2*8:
+                        im3[row, col, d] = b
+                        return im3
+                im3[row, col, d] = b
+    return im3
+
+def lsb_vr_reveal(im, hidden_shape, alpha=9, max_p=4):
+    h1, w1, d1 = im.shape
+    h2, w2, d2 = hidden_shape
+        
+    res = np.zeros(hidden_shape, dtype=int)
+    
+    im2_ctr = 0
+    res_b = 0
+    
+    for row in range(2, h1 - 1):
+        for col in range(2 + row%2, w1 - 1, 2):
+            for d in range(d1):
+                x = im1[row - 1, col, d] ^ im1[row + 1, col, d] ^ im1[row, col - 1, d] ^ im1[row, col + 1, d]
+                p = 1
+                if x > alpha:
+                    p = min(max_p, int(np.ceil(x/2)))
+                
+                b = im[row, col, d]
+                for j in range(p - 1, -1, -1):
+                    b2, z2, y2, x2 = coords_from_ctr(im2_ctr, h2, w2, d2)
+                    res_b = res_b | (((b>>j)&1) << (7 - b2)) # Take j-th LSB of b and insert it at b2-th MSG of res_b
+                    
+                    im2_ctr += 1
+                    if b2 == 7:
+                        res[x2, y2, z2] = res_b
+                        res_b = 0
+                    if im2_ctr == h2*w2*d2*8:
+                        return res
+                                    
+    return res
